59d9c7f4363392cbdca6a9781f94075f933373ab
Zvontsov Sergey <zvoncov@gmail.com>
2023-02-06 13:16:26 +0300

    create second VC UI

--- Tipsy/Controller/ResultViewController.swift
+++ Tipsy/Controller/ResultViewController.swift
@@ -9,11 +9,92 @@ import UIKit
 
 class ResultViewController: UIViewController {
 
+    //MARK: - Variable
+    
+    var percentage: Double?
+    var split: Int?
+    var result: Double?
+    
+    var labels = LabelProperty()
+    var buttons = ButtonsProperty()
+    
+    //MARK: - UIView and UIStackView
+    
+    lazy var resultView: UIView = {
+        let view = UIView()
+        view.backgroundColor = #colorLiteral(red: 0.8431372549, green: 0.9764705882, blue: 0.9215686275, alpha: 1)
+        view.addSubview(totalResultLabel)
+        view.addSubview(totalResultValueLabel)
+        return view
+    }()
+    
+    //MARK: - UIButton
+    
+    lazy var recalculateButton: UIButton = {
+        let button = UIButton(frame: buttons.calcButtonFrame)
+        button.setTitle("Recalculate", for: .normal)
+        button.backgroundColor = buttons.calcButtonColor
+        button.setTitleColor(buttons.calcButtonTextColor, for: .normal)
+        button.titleLabel?.font = buttons.calcButtonFont
+        button.titleLabel?.numberOfLines = 1
+        button.addTarget(self, action: #selector(recalcButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    //MARK: - UILabel
+    
+    lazy var totalResultLabel: UILabel = {
+        let label = UILabel(frame: labels.totalResultTitleLabelFrame)
+        label.text = "Total per person"
+        label.textAlignment = .center
+        label.font = labels.totalResultTitleLabelFont
+        label.textColor = labels.totalResultTitleTextColor
+        return label
+    }()
+    
+    lazy var totalResultValueLabel: UILabel = {
+        let label = UILabel(frame: labels.totalResultValueLabelFrame)
+        label.text = String(result ?? 0)
+        label.textAlignment = .center
+        label.font = labels.totalResultValueLabelFont
+        label.textColor = labels.totalResultValueTextColor
+        label.numberOfLines = 1
+        return label
+    }()
+    
+    lazy var settingsTitleLabel: UILabel = {
+        let label = UILabel(frame: labels.settingsTitleLabelFrame)
+        label.text = "Split between \(split ?? 0) people, with \(percentage ?? 0)% tip."
+        label.textAlignment = .center
+        label.font = labels.settingsTitleLabelFont
+        label.textColor = labels.settingsTitleLabelTextColor
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    //MARK: - Other UI elements
+    
+    //MARK: - ViewDidLoad
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        view.addSubview(resultView)
+        view.addSubview(settingsTitleLabel)
+        view.addSubview(recalculateButton)
+        setupConstraints()
+        
     }
+    
+    //MARK: - Setup Constraints
 
+    func setupConstraints() {
+        
+    }
+    
+    //MARK: - Methods
+    
+    @objc func recalcButtonTapped(_ sender: UIButton) {
+        
+    }
 
 }
--- Tipsy/Services/LabelsProperty.swift
+++ Tipsy/Services/LabelsProperty.swift
@@ -25,4 +25,16 @@ struct LabelProperty {
     let splitValueTitleLabelFont: UIFont = UIFont.systemFont(ofSize: 35.0, weight: .regular)
     let splitValueTitleTextColor: UIColor = #colorLiteral(red: 0, green: 0.6901960784, blue: 0.4196078431, alpha: 1)
     
+    let totalResultTitleLabelFrame: CGRect = CGRect(x: 0, y: 0, width: 414, height: 36)
+    let totalResultTitleLabelFont: UIFont = UIFont.systemFont(ofSize: 30.0, weight: .regular)
+    let totalResultTitleTextColor: UIColor = #colorLiteral(red: 0.6666666865, green: 0.6666666865, blue: 0.6666666865, alpha: 1)
+    
+    let totalResultValueLabelFrame: CGRect = CGRect(x: 0, y: 0, width: 414, height: 100)
+    let totalResultValueLabelFont: UIFont = UIFont.systemFont(ofSize: 45.0, weight: .bold)
+    let totalResultValueTextColor: UIColor = #colorLiteral(red: 0, green: 0.6901960784, blue: 0.4196078431, alpha: 1)
+    
+    let settingsTitleLabelFrame: CGRect = CGRect(x: 0, y: 0, width: 314, height: 117)
+    let settingsTitleLabelFont: UIFont = UIFont.systemFont(ofSize: 25.0, weight: .regular)
+    let settingsTitleLabelTextColor: UIColor = #colorLiteral(red: 0.6666666865, green: 0.6666666865, blue: 0.6666666865, alpha: 1)
+    
 }
